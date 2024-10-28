dbf2b8818d7eb9511f33d6877a7261ade85e60b4
pntbr <pntbr@pm.me>
2024-10-28 17:44:19 +0100

    ajoute du css et nunjunk

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,5 +2,10 @@ export const config = {
   markdownTemplateEngine: "njk",
   htmlTemplateEngine: "njk",
   dataTemplateEngine: "njk",
-  templateFormats: ["njk", "md", "html"]
-};
+  templateFormats: ["njk", "md", "html"],
+}
+
+export default function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("bundle.css")
+}
+
