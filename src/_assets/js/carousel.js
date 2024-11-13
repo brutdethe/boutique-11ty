0ef16f72c02294888525419c5b005a147d1781b9
pntbr <pntbr@pm.me>
2024-11-13 14:53:16 +0100

    installe une version simplifiée du css splide

---
+++ src/_assets/js/carousel.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const splide = new Splide('#card-carousel', {
+        pagination: false
+    })
+
+    const thumbnails = document.getElementsByClassName('card-thumbnail')
+    let current
+
+    for (let i = 0; i < thumbnails.length; i++) {
+        initThumbnail(thumbnails[i], i)
+    }
+
+    function initThumbnail(thumbnail, index) {
+        thumbnail.addEventListener('click', function () {
+            splide.go(index)
+        })
+    }
+
+    splide.on('mounted move', function () {
+        const thumbnail = thumbnails[splide.index]
+
+        if (thumbnail) {
+            if (current) {
+                current.classList.remove('is-active')
+            }
+
+            thumbnail.classList.add('is-active')
+            current = thumbnail
+        }
+    })
+
+    splide.mount()
+})
