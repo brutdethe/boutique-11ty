0fcc3ac5d41d8ad63dd43ac12ff92675fcc0cd1c
pntbr <pntbr@pm.me>
2024-11-16 00:38:45 +0100

    simplifie add_cart et change buyed pour disabled

--- src/_assets/js/add_cart.js
+++ src/_assets/js/add_cart.js
@@ -1,29 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     updateCartBadge()
 
-    const addToCartButtons = document.querySelectorAll('.add-to-cart')
     const cart = storage.getCart()
 
-    addToCartButtons.forEach((button) => {
+    document.querySelectorAll('.add-to-cart').forEach((button) => {
         const productElement = button.closest('.product-infos')
         const productId = productElement.dataset.id
 
-        const isProductInCart = cart.some((item) => item.id === productId)
-
-        if (isProductInCart) {
-            button.classList.add('buyed')
-            button.textContent = 'Déjà ajouté'
+        if (cart.some((item) => item.id === productId)) {
+            disableButton(button)
         } else {
-            button.addEventListener('click', function (event) {
+            button.addEventListener('click', (event) => {
                 event.preventDefault()
-
-                const product = {
-                    id: productElement.dataset.id,
-                    qty: 1
-                }
-
-                addToCart(product)
-                button.classList.add('buyed')
+                addToCart({ id: productId, qty: 1 })
+                disableButton(button)
             })
         }
     })
@@ -31,5 +21,11 @@ document.addEventListener('DOMContentLoaded', function () {
     function addToCart(product) {
         cart.push(product)
         storage.setCart(cart)
+    }
+
+    function disableButton(button) {
+        button.classList.add('disabled')
+        button.setAttribute('disabled', 'disabled')
+        button.textContent = 'Déjà ajouté'
     }
 })
